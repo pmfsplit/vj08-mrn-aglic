8ade40d5af95ac67a1a48f7929163e401739acd6
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2021-03-30 16:18:00 +0200

    Added actor system creation
    Kreiramo 4 actor sustava koristeći petlju. Učitamo konfig i dodijelimo port.

--- DistributedPubSub/DistributedPubSub/Program.cs
+++ DistributedPubSub/DistributedPubSub/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using Akka.Actor;
+using Akka.Configuration;
 using AkkaConfigProvider;
 
 namespace DistribPubSub
@@ -10,6 +13,20 @@ namespace DistribPubSub
             var configProvider = new ConfigProvider();
             var config = configProvider.GetAkkaConfig<MyAkkaConfig>();
 
+            var ports = new[] {12000, 12001};
+            
+            // Pokreniti ćemo 4 čvora unutar ovog jednog projekta da vidimo da se i to može.
+            // Važno je da kreiramo 4 actor sustava
+            foreach (var el in Enumerable.Range(0, 4))
+            {
+                var port = el > ports.Length ? 0 : ports[el];
+                var akkaConfig = ConfigurationFactory
+                    .ParseString($"akka.remote.dot-netty.tcp.port={port}")
+                    .WithFallback(config);
+                var system = ActorSystem.Create("DisPubSubExample", config);
+                var props = Props.Create(() => new ChatActor());
+                system.ActorOf(props);
+            }
 
             Console.WriteLine("Hello World!");
         }
